18ba900489fa37185edf8797b2b9a535cb0fbd8b
Evan Jarrett <ejfirestar00@gmail.com>
2016-10-30 18:16:49 -0700

    add table sorting and finish command tables

--- site.js
+++ site.js
@@ -1,10 +1,9 @@
-
 /* https://medium.com/@_jh3y/b30d49f9a0ff */
-(function () {
+speeddial = function () {
     var VISIBLE_CLASS = "is-showing-options",
-        fab_btn  = document.getElementById("fab_btn"),
-        fab_ctn  = document.getElementById("fab_ctn"),
-        showOpts = function(e) {
+        fab_btn = document.getElementById("fab_btn"),
+        fab_ctn = document.getElementById("fab_ctn"),
+        showOpts = function (e) {
             var processClick = function (evt) {
                 if (e !== evt) {
                     fab_ctn.classList.remove(VISIBLE_CLASS);
@@ -20,10 +19,36 @@
         };
     fab_btn.addEventListener("mouseover", showOpts);
     fab_btn.addEventListener("click", showOpts);
-}.call(this));
+};
 
-$('#return-to-top').click(function() {
-    $('body,html').animate({
-        scrollTop : 0
-    }, 500);
-});
+hideColumns = function () {
+    var tables = document.getElementsByClassName("tny-table");
+    for (var t = 0, table; table = tables[t]; t++) {
+        for (var i = 0, row; row = table.rows[i]; i++) {
+            for (var j = 0, col; col = row.cells[j]; j++) {
+                if (j == 0) {
+                    col.style.width = "140px";
+                }
+                if (j == 1) {
+                    col.style.width = "180px";
+                }
+                if (j > 1) {
+                    col.className += " mdl-cell--hide-phone";
+                }
+                if (j > 2) {
+                    col.className += " mdl-cell--hide-tablet";
+                }
+            }
+        }
+    }
+};
+
+speeddial();
+hideColumns();
+
+var options = {
+        valueNames: ['commands', 'permissions']
+    };
+var utilsTable = new List('utils-table', options).sort('commands', { order: "asc" });
+var rolesTable = new List('roles-table', options).sort('commands', { order: "asc" });
+var notifsTable = new List('notifs-table', options).sort('commands', { order: "asc" });
